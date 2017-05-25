08c3ee3ccd5adc1165b8e7180ccc24fac3dc888f
Orgz1AAA <dthomassmith@marshalltown.k12.ia.us>
2017-05-25 14:41:49 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,40 @@
-
+function generate1() {
+  var gen = Math.floor(Math.random() * 10);
+  document.getElementById("ten").innerHTML = gen;
+}
+function generate2() {
+  var gen = Math.floor(Math.random() * 20);
+  document.getElementById("ten").innerHTML = gen;
+}
+function generate3() {
+  var gen = Math.floor(Math.random() * 30);
+  document.getElementById("ten").innerHTML = gen;
+}
+function generate4() {
+  var gen = Math.floor(Math.random() * 40);
+  document.getElementById("ten").innerHTML = gen;
+}
+function generate5() {
+  var gen = Math.floor(Math.random() * 50);
+  document.getElementById("ten").innerHTML = gen;
+}
+function generate6() {
+  var gen = Math.floor(Math.random() * 60);
+  document.getElementById("ten").innerHTML = gen;
+}
+function generate7() {
+  var gen = Math.floor(Math.random() * 70);
+  document.getElementById("ten").innerHTML = gen;
+}
+function generate8() {
+  var gen = Math.floor(Math.random() * 80);
+  document.getElementById("ten").innerHTML = gen;
+}
+function generate9() {
+  var gen = Math.floor(Math.random() * 90);
+  document.getElementById("ten").innerHTML = gen;
+}
+function generate10() {
+  var gen = Math.floor(Math.random() * 100);
+  document.getElementById("ten").innerHTML = gen;
+}
